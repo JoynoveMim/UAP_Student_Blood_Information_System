b40b1f5d3ae4d5f289a476e20ee3cceaff2c0b00
Fabia <fabiarhmn@gmail.com>
2025-10-18 12:48:36 +0600

    update

---
+++ static/js/location_map.js
@@ -0,0 +1,10 @@
+// Simple map implementation using Leaflet or Google Maps
+function initLocationMap() {
+    // Implementation for map interface
+    // Users can pin their location
+    // Convert address to coordinates
+}
+
+function calculateDistance(lat1, lng1, lat2, lng2) {
+    // Client-side distance calculation
+}

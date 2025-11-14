140dd99d28a1dd202c12c4b80670eb5d5306aaa2
tasnim33-droid <tasnimsanim33@gmail.com>
2025-11-14 20:25:50 +0600

    update

--- UAP_Student_Blood_Information_System/blood_system/asgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from django.core.asgi import get_asgi_application
-
-# Use the full path to your settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UAP_Student_Blood_Information_System.bloodbank.settings')
-
-application = get_asgi_application()
